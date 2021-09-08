00eb16fa81bbbfea3c01dc6c4abea31fb560cec2
SouOka <oka.soichiro@donuts.ne.jp>
2021-09-08 20:27:09 +0900

    feat: エンコーダーのチェック処理を修正

--- encoder/src/main/java/com/pedro/encoder/utils/CodecUtil.java
+++ encoder/src/main/java/com/pedro/encoder/utils/CodecUtil.java
@@ -318,10 +318,6 @@ public class CodecUtil {
      */
     private static boolean isValid(String name) {
         //This encoder is invalid and produce errors (Only found in AVD API 16)
-        if (name.equalsIgnoreCase("aacencoder")) return false;
-        // 一部エンコーダーで配信ができなかったため、該当エンコーダーのチェックを外す
-        
-        return (!name.equals("OMX.qcom.video.encoder.avc") &&
-            !name.equals("c2.qti.avc.encoder")) || !Build.MODEL.startsWith("Pixel");
+        return !name.equalsIgnoreCase("aacencoder");
     }
 }
