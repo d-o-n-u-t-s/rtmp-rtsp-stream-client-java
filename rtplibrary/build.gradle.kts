a5315cd0d1af4b9f4fc9620c3e62dc10dc6a6527
kanaya.akari <akari317017@gmail.com>
2023-01-02 19:17:52 +0900

    ref:rtmpとstplibraryの置き換え

---
+++ rtplibrary/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.library")
+}
+group = "com.github.pedroSG94"
+
+val COMPILE_SDK: String by project
+val MIN_SDK_VERSION: String by project
+val TARGET_SDK_VERSION: String by project
+
+android {
+    compileSdk = COMPILE_SDK.toInt()
+    
+    defaultConfig {
+        minSdk = MIN_SDK_VERSION.toInt()
+        targetSdk = TARGET_SDK_VERSION.toInt()
+    }
+    
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            consumerProguardFiles("proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    api(project(":encoder"))
+    api(project(":rtmp"))
+    api(project(":rtsp"))
+}
