eb28ff8af4f2afc065c178c291ac761537b95860
watanabe.koto <watanabe.koto@donuts.ne.jp>
2025-07-17 16:29:44 +0900

    feat: 音声合成用interfaceを追加

--- encoder/src/main/java/com/pedro/encoder/input/audio/NoAudioEffect.java
+++ encoder/src/main/java/com/pedro/encoder/input/audio/NoAudioEffect.java
@@ -1,9 +1,14 @@
 package com.pedro.encoder.input.audio;
 
 public class NoAudioEffect extends CustomAudioEffect {
-
-  @Override
-  public byte[] process(byte[] pcmBuffer) {
-    return pcmBuffer;
-  }
+    
+    @Override
+    public byte[] process(byte[] pcmBuffer) {
+        return pcmBuffer;
+    }
+    
+    @Override
+    public byte[] process(byte[] pcmBuffer, byte[] internalPcmBuffer) {
+        return pcmBuffer;
+    }
 }
