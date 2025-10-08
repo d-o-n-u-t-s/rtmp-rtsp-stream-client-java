868044b904628e9bf9804195c85ab9ce3f7f8c28
MakotoWatanabe <166352957+donuts-watanabemakoto@users.noreply.github.com>
2025-10-08 11:31:30 +0900

    Revert "ミュート機能テスト実装"
    This reverts commit 38f2719d0ce6f13a097277d1b4d5e6c666f94bd4.

--- encoder/src/main/java/com/pedro/encoder/input/audio/MicrophoneManagerManual.java
+++ encoder/src/main/java/com/pedro/encoder/input/audio/MicrophoneManagerManual.java
@@ -1,6 +1,5 @@
 package com.pedro.encoder.input.audio;
 
-import android.os.Debug;
 import android.os.HandlerThread;
 import android.util.Log;
 
@@ -32,7 +31,6 @@ public class MicrophoneManagerManual extends MicrophoneManager implements GetFra
     if (audioRecord != null) {
       audioRecord.startRecording();
       running = true;
-      initAudioBuffer();
       Log.i(TAG, "Microphone started");
     } else {
       Log.e(TAG, "Error starting, microphone was stopped or not created, "
@@ -65,24 +63,9 @@ public class MicrophoneManagerManual extends MicrophoneManager implements GetFra
 
   @Override
   public Frame getInputFrame() {
-    try {
-      pcmBuffer.rewind();
-      if (audioRecord == null) return null;
-      int readSize = audioRecord.read(pcmBuffer, pcmBuffer.remaining());
-      Log.d(TAG, "readSize: " + readSize);
-      if (readSize < 0) return null;
-      byte[] audioBufferArray = audioBuffer.array();
-      System.arraycopy(audioBufferArray, readSize, audioBufferArray, 0, audioBufferSize - readSize);
-
-      pcmBuffer.get(audioBufferArray, audioBufferSize - readSize, readSize);
-      ByteBuffer emitFrameBuffer = ByteBuffer.allocate(readSize);
-      emitFrameBuffer.put(audioBufferArray, 0, readSize);
-
-      return new Frame(muted ? pcmBufferMuted : customAudioEffect.process(pcmBuffer.array()),
-              muted ? 0 : pcmBuffer.arrayOffset(), readSize);
-    } catch (Exception e) {
-      Log.e(TAG, "Error getting frame", e);
-      return null;
-    }
+    int size = audioRecord.read(pcmBuffer, pcmBuffer.remaining());
+    if (size < 0) return null;
+    return new Frame(muted ? pcmBufferMuted : customAudioEffect.process(pcmBuffer.array()),
+        muted ? 0 : pcmBuffer.arrayOffset(), size);
   }
 }
--- rtplibrary/src/main/java/com/pedro/rtplibrary/base/DisplayBase.java
+++ rtplibrary/src/main/java/com/pedro/rtplibrary/base/DisplayBase.java
@@ -54,7 +54,7 @@ import static android.content.Context.MEDIA_PROJECTION_SERVICE;
  */
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public abstract class DisplayBase implements GetAacData, GetVideoData, GetMicrophoneData {
-  
+
   private OffScreenGlThread glInterface;
   protected Context context;
   private MediaProjection mediaProjection;
